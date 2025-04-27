b1f568a69c66047c4bbda624e597682c8f77ee03
Jana Nasser <95519431+jana7890@users.noreply.github.com>
2025-04-27 12:09:07 +0300

    Add LibraryManagementSystem/src/Main.java 1745744947873

---
+++ LibraryManagementSystem/src/Main.java
@@ -0,0 +1,12 @@
+package librarymanagementsystem;
+
+import javax.swing.SwingUtilities;
+
+public class Main {
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(() -> {
+            LibraryGUI gui = new LibraryGUI();
+            gui.setVisible(true);
+        });
+    }
+}
